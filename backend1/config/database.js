3b500ed3f956fb3f7a7b0781522f5d63c50d6d1b
Mayank Raj <127421631+mnkraj@users.noreply.github.com>
2024-02-29 20:54:27 +0530

    Update database.js

--- backend1/config/database.js
+++ backend1/config/database.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const connectDB = async () => {
   try {
 
-  const conn = await mongoose.connect("mongodb+srv://shishujsrsidh:MYjytQpDrpvKAHFo@cluster0.jz8o0jn.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0", {
+  const conn = await mongoose.connect("mongodb+srv://shishujsrsidh:ejMjnBpa8CDfk5nQ@cluster0.9nmxwc2.mongodb.net/Admin_gdsc?retryWrites=true&w=majority&appName=Cluster0", {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
