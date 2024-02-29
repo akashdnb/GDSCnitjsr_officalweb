2911e2cf3a4916ec5df4a1dbf91fa41821453a16
SIDHARTH SHARMA <shishu.jsrsdih@gmail.com>
2024-03-01 01:05:44 +0530

    aa

--- backend1/config/verifyToken.js
+++ backend1/config/verifyToken.js
@@ -3,7 +3,7 @@ const jwt = require("jsonwebtoken");
 const verifyToken = async (req, res) => {
   try {
     const { token } = req.body;
-    const result = await jwt.verify(token, process.env.JWT_SECRET);
+    const result = await jwt.verify(token, 'DGQwJ_qtHfmP6gURxFf-TplURR_JNzXk3P_uy2paU65vhsB8zs88iYV80L33v7YA');
     res.status(200).json({
       result,
     });
