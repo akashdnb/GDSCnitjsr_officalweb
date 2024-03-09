690bbf757907b285c75bb0276c2f6092133f1240
Harshit Shrivastav <146455282+harshitnitjsr@users.noreply.github.com>
2024-03-09 15:20:19 +0530

    Update cloudnary.js

--- backend1/utils/cloudnary.js
+++ backend1/utils/cloudnary.js
@@ -1,11 +1,11 @@
 const cloudinary=require("cloudinary").v2
 // otenv"
 // dotenv.config()
-require('dotenv').config({path:'../.env'});        
+          
 cloudinary.config({ 
-  cloud_name: process.env.cLOUD_NAME, 
-  api_key: process.env.API_KEY, 
-  api_secret: process.env.API_SECRET
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME, 
+  api_key: process.env.CLOUDINARY_API_KEY, 
+  api_secret: process.env.CLOUDINARY_API_SECRET
 });
 
 
