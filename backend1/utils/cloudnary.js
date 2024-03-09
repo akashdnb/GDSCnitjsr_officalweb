c45dc4d0cd584028ea39f728245fbc0a015c444b
SIDHARTH SHARMA <shishu.jsrsdih@gmail.com>
2024-03-09 21:16:00 +0530

    rever

--- backend1/utils/cloudnary.js
+++ backend1/utils/cloudnary.js
@@ -3,15 +3,9 @@ const cloudinary=require("cloudinary").v2
 // dotenv.config()
 require('dotenv').config({path:'../.env'});        
 cloudinary.config({ 
-<<<<<<< HEAD
-  cloud_name: process.env.cLOUD_NAME, 
-  api_key: process.env.API_KEY, 
-  api_secret: process.env.API_SECRET
-=======
   cloud_name:"dmo1egafg", 
   api_key: "179955851864828", 
   api_secret:"_yi7Ev9EyvfjCuye__J65BQPdbA"
->>>>>>> 87b60025a0fa906bef7c23d660b27b53f68d77ca
 });
 
 
