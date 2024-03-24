afbee3807f5c4b6c5c8191ab98f1d0f8cc42648c
SIDHARTH SHARMA <shishu.jsrsdih@gmail.com>
2024-03-25 01:39:21 +0530

    LInk Updated

--- gdscnitjsr1/src/components/hero.js
+++ gdscnitjsr1/src/components/hero.js
@@ -16,11 +16,11 @@ function Hero() {
         </div>
             
         <div className="sm:w-[50%] z-[2] flex items-end">
-            <img src={HeroImage}/>
+            <img src={HeroImage} alt='img'/>
         </div>
         </div>
         <div className='sm:w-[245.1px] sm:h-[71.21px] w-[50%] h-fit p-[10px] bg-[#4285F4] rounded-[20px] text-white sm:text-[24px] text-[16px] flex items-center justify-center mt-10'>
-              <a href='https://gdsc.community.dev/national-institute-of-technology-nit-jamshedpur/' target="_blank" rel="noopener noreferrer">Join the Chapter</a> 
+              <a href="https://gdsc.community.dev/national-institute-of-technology-nit-jamshedpur/" target="_blank" rel="noopener noreferrer">Join the Chapter</a> 
             </div>
         
       </div>
